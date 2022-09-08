909b9067d8cedb6980fbd515806913cafb14ba20
Gabriel Odewale <gharbzlaw@gmail.com>
2022-09-08 07:37:23 -0700

    sixth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,5 +3,10 @@
  * main - Entry point
  *
  * Return: always 0
- */                                                        int main (void)                                            {                                                                  puts("\"Programming is like building a multilingual puzzle"                                                   );
-        return (0);                                        }
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"
+);
+	return (0);
+}
