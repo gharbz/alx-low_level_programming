a663e8c4b4a891da4c32fad1cfe1a2001e431e7d
Gabriel Odewale <gharbzlaw@gmail.com>
2022-09-09 11:11:57 -0700

    Leggoos

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point<F11>
+ *
+ * Description: print all single digit number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+
+	return (0);
+
+}
