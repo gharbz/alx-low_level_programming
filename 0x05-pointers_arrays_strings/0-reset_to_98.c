184c04e41820edc5f7afecc4c9322cd30d1e374c
Gabriel Odewale <gharbzlaw@gmail.com>
2022-09-19 15:16:13 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ *               qnd updates the value it points to 98
+ * 
+ * @n: int parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
